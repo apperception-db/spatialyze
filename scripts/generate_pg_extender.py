39fc4d591d888c2b2b9f1f98ff31321a1071ab4f
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2023-08-06 18:36:10 -0700

    Update generate_pg_extender.py

--- scripts/generate_pg_extender.py
+++ scripts/generate_pg_extender.py
@@ -3,8 +3,8 @@ import os
 with open('./install.sql', 'w') as f:
     f.write('\n'.join(map(
         lambda x: f'\i {x};',
-        filter(
+        sorted(filter(
             lambda x: x.endswith('sql') and x != 'install.sql',
             os.listdir('.'),
-        ),
+        )),
     )))
