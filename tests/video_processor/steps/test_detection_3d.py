737cbd28f1107aa0922bcb620c72dd04dd1549f2
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-08-13 15:18:43 -0700

    save test

--- tests/video_processor/steps/test_detection_3d.py
+++ tests/video_processor/steps/test_detection_3d.py
@@ -47,10 +47,10 @@ def test_detection_3d():
         det_result = FromDetection2DAndRoad.get(output)
         assert det_result is not None
 
-        # with open(os.path.join(OUTPUT_DIR, f'FromDetection2DAndRoad--{name}.json'), 'w') as f:
-        #     json.dump([(d[0].cpu().numpy().tolist(), d[1], d[2]) for d in det_result], f, indent=1)
-        # with open(os.path.join(OUTPUT_DIR, f'FromDetection2DAndRoad--{name}.pkl'), 'wb') as f:
-        #     pickle.dump([(d[0].cpu(), d[1], d[2]) for d in det_result], f)
+        with open(os.path.join(OUTPUT_DIR, f'FromDetection2DAndRoad--{name}.json'), 'w') as f:
+            json.dump([(d[0].cpu().numpy().tolist(), d[1], d[2]) for d in det_result], f, indent=1)
+        with open(os.path.join(OUTPUT_DIR, f'FromDetection2DAndRoad--{name}.pkl'), 'wb') as f:
+            pickle.dump([(d[0].cpu(), d[1], d[2]) for d in det_result], f)
 
         with open(os.path.join(OUTPUT_DIR, f'FromDetection2DAndRoad--{name}.pkl'), 'rb') as f:
             det_groundtruth = pickle.load(f)
