1eae15cf56d2d55640e334a0cc5af7bf05052751
yousefh409 <yousefh409@gmail.com>
2023-08-13 22:51:03 -0700

    interpolation left

--- spatialyze/utils/save_video_util.py
+++ spatialyze/utils/save_video_util.py
@@ -7,23 +7,27 @@ from ..video_processor.stages.tracking_3d.tracking_3d import Tracking3DResult
 def save_video_util(
     objects: "dict[str, list[tuple]]",
     trackings: "dict[str, list[T3DMetadatum]]",
-    VIDEO_PATH: "str",
     OUTPUT_PATH: "str",
-    is_bbox: "bool" = False,
-):
+    addBoundingBoxes: "bool" = False,
+) -> "list[tuple[str, int]]":
     frame_trackings = _get_frame_objects(trackings)
     cameraIds = _get_camera_ids(objects)
 
+    result: "list[tuple[str, int]]" = []
+
     for videoname, frame_tracking in frame_trackings.items():
         cameraId = cameraIds[videoname]
-        video_file = VIDEO_PATH + videoname
         output_file = OUTPUT_PATH + cameraId + "-result.mp4"
 
-        cap = cv2.VideoCapture(video_file)
+        cap = cv2.VideoCapture(videoname)
+        if not cap.isOpened():
+            print(f"WARNING: Cannot read video file: {videoname}")
+            continue
+
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         vid_writer = cv2.VideoWriter(
-            output_file, cv2.VideoWriter_fourcc("m", "p", "4", "v"), 30, (width, height)
+            output_file, cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height)
         )
 
         frame_cnt = 0
@@ -32,24 +36,30 @@ def save_video_util(
             if not ret:
                 break
 
-            if is_bbox:
-                for track in frame_tracking.get(frame_cnt, []):
-                    bbox_left, bbox_top, bbox_w, bbox_h = (
-                        track.bbox_left,
-                        track.bbox_top,
-                        track.bbox_w,
-                        track.bbox_h,
-                    )
-                    x1, y1 = bbox_left, bbox_top
-                    x2, y2 = bbox_left + bbox_w, bbox_top - bbox_h
-                    frame = cv2.rectangle(
-                        frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 0), 2
-                    )
+            if frame_cnt in frame_tracking:
+                if addBoundingBoxes:
+                    for track in frame_tracking.get(frame_cnt, []):
+                        bbox_left, bbox_top, bbox_w, bbox_h = (
+                            track.bbox_left,
+                            track.bbox_top,
+                            track.bbox_w,
+                            track.bbox_h,
+                        )
+                        x1, y1 = bbox_left, bbox_top
+                        x2, y2 = bbox_left + bbox_w, bbox_top - bbox_h
+                        frame = cv2.rectangle(
+                            frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 255, 0), 2
+                        )
 
-            vid_writer.write(frame)
+                vid_writer.write(frame)
+                result.append((videoname, frame_cnt))
+
             frame_cnt += 1
 
         vid_writer.release()
+
+    return result
+    
 
 
 def _get_frame_objects(trackings: "dict[str, list[T3DMetadatum]]"):
