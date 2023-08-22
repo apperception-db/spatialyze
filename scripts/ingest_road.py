506317f3767e6b1f635fdacb6b1ba90c2c36677b
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2023-08-21 20:35:44 -0700

    fix

--- scripts/ingest_road.py
+++ scripts/ingest_road.py
@@ -1,4 +1,4 @@
-from spatialyze.utils.ingest_road import ingest_road
+from spatialyze.utils import ingest_road
 from spatialyze.database import database
 
-ingest_road(database, '../data/scenic/road-network/boston-seaport/')
+ingest_road(database, './data/scenic/road-network/boston-seaport')
