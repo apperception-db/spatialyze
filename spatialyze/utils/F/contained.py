874d5515c5e7a359aa10dcd42444c74fd5e1b9db
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-09-08 13:54:22 -0700

    update helper function: contained

--- spatialyze/utils/F/contained.py
+++ spatialyze/utils/F/contained.py
@@ -9,7 +9,7 @@ from ...predicate import (
 
 @call_node
 def contained(visitor: "GenSqlVisitor", args: "list[PredicateNode]"):
-    object, region = args
+    object, region = args[:2]
     if isinstance(object, ObjectTableNode):
         object = object.traj @ camera.time
-    return f"contained({visitor(object)},{visitor(region)})"
+    return f"contained({visitor(object)}, {visitor(region)})"
