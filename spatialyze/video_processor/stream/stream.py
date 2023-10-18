3478823b72adcae2107b8a6100a287f1e9e3ef06
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-10-18 10:59:41 -0700

    test stream

--- spatialyze/video_processor/stream/stream.py
+++ spatialyze/video_processor/stream/stream.py
@@ -9,8 +9,8 @@ T = TypeVar("T")
 
 
 class Stream(Generic[T], ABC):
-    __use_count: int
-    __use_progress: list[int]
+    __stream_count: int
+    __stream_progress: list[int]
 
     __front: int
     __results: list[T | Skip | None]
@@ -21,46 +21,69 @@ class Stream(Generic[T], ABC):
         instance = super(Stream, cls).__new__(cls)
         for arg in args + tuple(kwargs.values()):
             if isinstance(arg, Stream):
-                arg.__use_progress.append(0)
-        instance.__use_count = 0
-        instance.__use_progress = []
+                arg.__stream_progress.append(0)
+        instance.__stream_count = 0
+        instance.__stream_progress = []
         instance.__front = 0
         instance.__results = []
         instance.__video = None
         instance.__iter_stream = None
         return instance
 
+    def execute(self, video: Video) -> list[T | Skip]:
+        self.__initialize_stream(video)
+        self.__initialize_stream_progress()
+        return list(self.stream(video))
+
     def stream(self, video: Video) -> Iterable[T | Skip]:
-        if self.__video is None or self.__iter_stream is None:
-            self.__video = video
-            self.__iter_stream = iter(self._stream(video))
-            self.__results.append(next(self.__iter_stream))
         assert self.__video == video
+        assert self.__iter_stream is not None
 
         idx = self.__assign_stream_idx()
         try:
             while True:
-                while len(self.__results) <= self.__use_progress[idx]:
+                while len(self.__results) <= self.__stream_progress[idx]:
                     self.__results.append(next(self.__iter_stream))
 
-                yield self.__results[self.__use_progress[idx]]
-                self.__use_progress[idx] += 1
+                result = self.__results[self.__stream_progress[idx]]
+                assert result is not None
+                yield result
+                self.__stream_progress[idx] += 1
 
                 self.__free_memory()
         except StopIteration:
             return
 
+    def __initialize_stream(self, video: Video):
+        self.__stream_progress = []
+        self.__stream_count = 0
+        self.__video = video
+        self.__iter_stream = iter(self._stream(video))
+        self.__results = [next(self.__iter_stream)]
+        self.__front = 0
+        for attr in dir(self):
+            stream = getattr(self, attr)
+            if isinstance(stream, Stream):
+                stream.__initialize_stream(video)
+
+    def __initialize_stream_progress(self):
+        self.__stream_progress.append(0)
+        for attr in dir(self):
+            stream = getattr(self, attr)
+            if isinstance(stream, Stream):
+                stream.__initialize_stream_progress()
+
     def __assign_stream_idx(self):
-        idx = self.__use_count
-        self.__use_count += 1
-        assert len(self.__use_progress) >= self.__use_count, (
-            len(self.__use_progress),
-            self.__use_count,
+        idx = self.__stream_count
+        self.__stream_count += 1
+        assert len(self.__stream_progress) >= self.__stream_count, (
+            len(self.__stream_progress),
+            self.__stream_count,
         )
         return idx
 
     def __free_memory(self):
-        _min_progress = min(self.__use_progress)
+        _min_progress = min(self.__stream_progress)
         while self.__front < _min_progress:
             self.__results[self.__front] = None
             self.__front += 1
