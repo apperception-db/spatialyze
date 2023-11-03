f95cb87adac98a19683e5605cd142eaa9a493e0d
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-11-03 15:11:01 +0000

    update

--- longvideo.py
+++ longvideo.py
@@ -49,35 +49,35 @@ else:
 # In[4]:
 
 
-NUSCENES_PROCESSED_DATA = "NUSCENES_PROCESSED_DATA"
-print(NUSCENES_PROCESSED_DATA in os.environ)
-print(os.environ['NUSCENES_PROCESSED_DATA'])
+# NUSCENES_PROCESSED_DATA = "NUSCENES_PROCESSED_DATA"
+# print(NUSCENES_PROCESSED_DATA in os.environ)
+# print(os.environ['NUSCENES_PROCESSED_DATA'])
 
 
-# In[12]:
+# # In[12]:
 
 
-DATA_DIR = os.environ[NUSCENES_PROCESSED_DATA]
-# with open(os.path.join(DATA_DIR, "videos", "frames.pkl"), "rb") as f:
-#     videos = pickle.load(f)
-with open(os.path.join(DATA_DIR, 'videos', 'videos.json'), 'r') as f:
-    videos = json.load(f)
-type(videos)
-videos[0]
+# DATA_DIR = os.environ[NUSCENES_PROCESSED_DATA]
+# # with open(os.path.join(DATA_DIR, "videos", "frames.pkl"), "rb") as f:
+# #     videos = pickle.load(f)
+# with open(os.path.join(DATA_DIR, 'videos', 'videos.json'), 'r') as f:
+#     videos = json.load(f)
+# type(videos)
+# videos[0]
 
 
 # In[10]:
 
 
-with open('./data/evaluation/video-samples/boston-seaport.txt', 'r') as f:
-    sampled_scenes = f.read().split('\n')
-print(sampled_scenes[0], sampled_scenes[-1], len(sampled_scenes))
+# with open('./data/evaluation/video-samples/boston-seaport.txt', 'r') as f:
+#     sampled_scenes = f.read().split('\n')
+# print(sampled_scenes[0], sampled_scenes[-1], len(sampled_scenes))
 
 
 # In[17]:
 
-
-with open(os.path.join(DATA_DIR, 'videos', 'boston-seaport-scene-' + sampled_scenes[0] + '-CAM_FRONT.pkl'), 'rb') as f:
+with open('/home/chanwutk/data/boston-seaport-scene-0655-CAM_FRONT.pkl', 'rb') as f:
+# with open(os.path.join(DATA_DIR, 'videos', 'boston-seaport-scene-' + sampled_scenes[0] + '-CAM_FRONT.pkl'), 'rb') as f:
     video = pickle.load(f)
 video_filename = video['filename']
 
@@ -92,7 +92,7 @@ video_filename
 
 
 frames = []
-cap = cv2.VideoCapture(os.path.join(DATA_DIR, 'videos', video_filename))
+cap = cv2.VideoCapture(os.path.join('/home/chanwutk/data/', video_filename))
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -104,15 +104,17 @@ cv2.destroyAllWindows()
 
 # In[28]:
 
+n = 633
 
-result = cv2.VideoWriter('./loop-video.avi',  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         12, (1600, 900)) 
-for i in tqdm(range(633)):
-    for frame in frames:
-        result.write(frame) 
-result.release() 
-cv2.destroyAllWindows()
+for i in [3]:
+    result = cv2.VideoWriter(f'./loop-video-{i}0.avi',  
+                            cv2.VideoWriter_fourcc(*'MJPG'), 
+                            20, (1600, 900)) 
+    for i in tqdm(range(n * i)):
+        for frame in frames:
+            result.write(frame) 
+    result.release() 
+    cv2.destroyAllWindows()
 
 
 # In[ ]:
