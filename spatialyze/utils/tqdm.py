55f07d86b3864309bf8083411caee0f83aadda3d
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-11-30 17:25:38 -0800

    fix tqdm

--- spatialyze/utils/tqdm.py
+++ spatialyze/utils/tqdm.py
@@ -1,6 +1,9 @@
 from .is_notebook import is_notebook
 
 if is_notebook():
-    pass
+    from tqdm.notebook import tqdm as _tqdm
 else:
-    pass
+    from tqdm.notebook import tqdm as _tqdm
+
+
+tqdm = _tqdm
