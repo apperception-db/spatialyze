0a2fc88696112a788db7dcbdb8442e15524860af
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-12-03 01:00:39 -0800

    fix dependencies

--- spatialyze/video_processor/stages/tracking/deepsort.py
+++ spatialyze/video_processor/stages/tracking/deepsort.py
@@ -1,13 +1,12 @@
 # import time
 from pathlib import Path
+import os
+import sys
 
 import torch
+import numpy as np
 
 from ...cache import cache
-from ...modules.yolo_deepsort.deep_sort.deep_sort import DeepSort
-from ...modules.yolo_deepsort.deep_sort.utils.parser import get_config
-from ...modules.yolo_deepsort.yolov5.utils.general import xyxy2xywh
-from ...modules.yolo_deepsort.yolov5.utils.torch_utils import select_device
 from ...payload import Payload
 from ...types import DetectionId
 from ..decode_frame.decode_frame import DecodeFrame
@@ -17,7 +16,52 @@ from .tracking import Tracking, TrackingResult
 FILE = Path(__file__).resolve()
 SPATIALYZE = FILE.parent.parent.parent.parent.parent
 WEIGHTS = SPATIALYZE / "weights"
-reid_weights = WEIGHTS / "osnet_x0_25_msmt17.pt"
+
+TORCHREID = SPATIALYZE / "spatialyze" / "video_processor" / "modules" / "yolo_deepsort" / "deep_sort" / "deep" / "reid"
+sys.path.append(str(TORCHREID))
+
+from ...modules.yolo_deepsort.deep_sort.deep_sort import DeepSort
+from ...modules.yolo_deepsort.deep_sort.utils.parser import get_config
+
+
+def xyxy2xywh(x):
+    # Convert nx4 boxes from [x1, y1, x2, y2] to [x, y, w, h] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+
+
+def select_device(device='', batch_size=0, newline=True):
+    # device = 'cpu' or '0' or '0,1,2,3'
+    # s = f'YOLOv5 🚀 torch {torch.__version__} '  # string
+    device = str(device).strip().lower().replace('cuda:', '')  # to string, 'cuda:0' to '0'
+    cpu = device == 'cpu'
+    if cpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # force torch.cuda.is_available() = False
+    elif device:  # non-cpu device requested
+        os.environ['CUDA_VISIBLE_DEVICES'] = device  # set environment variable
+        assert torch.cuda.is_available(), f'CUDA unavailable, invalid device {device} requested'  # check availability
+
+    cuda = not cpu and torch.cuda.is_available()
+    if cuda:
+        devices = device.split(',') if device else '0'  # range(torch.cuda.device_count())  # i.e. 0,1,6,7
+        n = len(devices)  # device count
+        if n > 1 and batch_size > 0:  # check batch_size is divisible by device_count
+            assert batch_size % n == 0, f'batch-size {batch_size} not multiple of GPU count {n}'
+        # space = ' ' * (len(s) + 1)
+        # for i, d in enumerate(devices):
+        #     p = torch.cuda.get_device_properties(i)
+        #     s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / 1024 ** 2:.0f}MiB)\n"  # bytes to MB
+    # else:
+    #     s += 'CPU\n'
+
+    # if not newline:
+    #     s = s.rstrip()
+    # LOGGER.info(s.encode().decode('ascii', 'ignore') if platform.system() == 'Windows' else s)  # emoji-safe
+    return torch.device('cuda:0' if cuda else 'cpu')
 
 
 class DeepSORT(Tracking):
