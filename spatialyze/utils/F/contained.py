c20e4ac07c77628d8d992c998e0fa052eb9c3378
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-12-17 17:39:47 -0800

    fix test

--- spatialyze/utils/F/contained.py
+++ spatialyze/utils/F/contained.py
@@ -5,7 +5,5 @@ from .common import default_location
 @call_node
 def contained(visitor: "GenSqlVisitor", args: "list[PredicateNode]"):
     object, region = args[:2]
-
     object = default_location(object)
-
-    return f"contained({visitor(object)}, {visitor(region)})"
+    return f"contained({visitor(object)},{visitor(region)})"
