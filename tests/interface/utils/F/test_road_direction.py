b4b33d569a32178442130b8014c69f962a90a8be
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-12-19 01:24:37 -0800

    fix road_direction

--- tests/interface/utils/F/test_road_direction.py
+++ tests/interface/utils/F/test_road_direction.py
@@ -4,13 +4,13 @@ from common import *
 
 @pytest.mark.parametrize("fn, sql", [
     (road_direction(o), 
-        "roadDirection(valueAtTimestamp(t0.trajCentroids,c0.timestamp),valueAtTimestamp(t0.itemHeadings,c0.timestamp))"),
+        "roadDirection(valueAtTimestamp(t0.trajCentroids,c0.timestamp),(valueAtTimestamp(t0.itemHeadings,c0.timestamp))::real)"),
     (road_direction(o, c.ego), 
-        "roadDirection(valueAtTimestamp(t0.trajCentroids,c0.timestamp),c0.egoHeading)"),
+        "roadDirection(valueAtTimestamp(t0.trajCentroids,c0.timestamp),(c0.egoHeading)::real)"),
     (road_direction(c), 
-        "roadDirection(c0.cameraTranslation,c0.cameraHeading)"),
+        "roadDirection(c0.cameraTranslation,(c0.cameraHeading)::real)"),
     (road_direction(c.ego, o), 
-        "roadDirection(c0.egoTranslation,valueAtTimestamp(t0.itemHeadings,c0.timestamp))"),
+        "roadDirection(c0.egoTranslation,(valueAtTimestamp(t0.itemHeadings,c0.timestamp))::real)"),
 ])
 def test_road_direction(fn, sql):
     assert gen(fn) == sql
