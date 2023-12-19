ad4d2cc30b049dbdb04cfc1f5f15cb1e620cb313
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-12-19 01:33:02 -0800

    fix text_ahead

--- tests/interface/utils/F/test_ahead.py
+++ tests/interface/utils/F/test_ahead.py
@@ -8,11 +8,11 @@ c = camera
 
 @pytest.mark.parametrize("fn, sql", [
     (ahead(o, c.ego), 
-        "ahead(valueAtTimestamp(t0.trajCentroids,c0.timestamp),c0.egoTranslation,c0.egoHeading)"),
+        "ahead(valueAtTimestamp(t0.trajCentroids,c0.timestamp),c0.egoTranslation,(c0.egoHeading)::real)"),
     (ahead(o, c.cam), 
-        "ahead(valueAtTimestamp(t0.trajCentroids,c0.timestamp),c0.cameraTranslation,c0.cameraHeading)"),
+        "ahead(valueAtTimestamp(t0.trajCentroids,c0.timestamp),c0.cameraTranslation,(c0.cameraHeading)::real)"),
     (ahead(o, o), 
-        "ahead(valueAtTimestamp(t0.trajCentroids,c0.timestamp),valueAtTimestamp(t0.trajCentroids,c0.timestamp),valueAtTimestamp(t0.itemHeadings,c0.timestamp))"),
+        "ahead(valueAtTimestamp(t0.trajCentroids,c0.timestamp),valueAtTimestamp(t0.trajCentroids,c0.timestamp),(valueAtTimestamp(t0.itemHeadings,c0.timestamp))::real)"),
 ])
 def test_ahead(fn, sql):
     assert gen(fn) == sql
