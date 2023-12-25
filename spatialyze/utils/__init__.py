71b9a296c5e820055053db9c619f0beb786fc21a
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-12-24 18:32:49 -0800

    clean up

--- spatialyze/utils/__init__.py
+++ spatialyze/utils/__init__.py
@@ -1,19 +1,13 @@
 from . import F
-from .compute_heading import compute_heading
 from .df_to_camera_config import df_to_camera_config
 from .export_tables import export_tables
 from .import_pickle import import_pickle
 from .ingest_road import ingest_road
-from .join import join
-from .transformation import transformation
 
 __all__ = [
     "F",
-    "compute_heading",
     "df_to_camera_config",
     "import_pickle",
     "export_tables",
-    "join",
-    "transformation",
     "ingest_road",
 ]
--- spatialyze/utils/join.py
+++
@@ -1,5 +0,0 @@
-from typing import Iterable
-
-
-def join(array: Iterable, delim: str = ","):
-    return delim.join(map(str, array))
