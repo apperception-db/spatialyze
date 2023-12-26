d5eeef50c4f07ff2f4027767792e49dbddb9960e
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-12-26 14:58:29 -0800

    fix saved file names

--- tests/video_processor/steps/test_detection_2d.py
+++ tests/video_processor/steps/test_detection_2d.py
@@ -64,7 +64,7 @@ def test_detection_2d():
         if os.environ.get('GENERATE_PROCESSOR_TEST_RESULTS', 'false') == 'true':
             with open(os.path.join(OUTPUT_DIR, f'YoloDetection--{name}.json'), 'w') as f:
                 json.dump([(d[0].cpu().numpy().tolist(), d[1], d[2]) for d in det_result], f, indent=1)
-        with open(os.path.join(OUTPUT_DIR, f'YoloDetection--{name}.json'), 'wb') as f:
+        with open(os.path.join(OUTPUT_DIR, f'YoloDetection--{name}.pkl'), 'wb') as f:
             pickle.dump([D2DMetadatum(det.detach().cpu(), clss, did) for det, clss, did in det_result], f)
 
         with open(os.path.join(OUTPUT_DIR, f'YoloDetection--{name}.json'), 'r') as f:
