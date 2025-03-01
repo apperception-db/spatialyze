5e19894c8846258426abdb243800902663995127
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2025-03-01 01:00:21 +0000

    min_distance

--- tests/engine/scenic/scenic_common.py
+++ tests/engine/scenic/scenic_common.py
@@ -5,3 +5,12 @@ def get_results(path: str) -> list[QueryResult]:
     with open(path, "r") as f:
         results = f.readlines()
     return eval("\n".join(results[1:]))
+
+
+def set_results(results: list, path: str):
+    with open(path, "w") as f:
+        f.write('from spatialyze.data_types.query_result import QueryResult\n')
+        f.write("[\n")
+        for r in results:
+            f.write(f"{r},\n")
+        f.write("]\n")
---
+++ tests/engine/scenic/test_min_distance.py
@@ -0,0 +1,22 @@
+from spatialyze.predicate import objects, camera, lit
+from spatialyze.utils import F
+from spatialyze.database import database
+import datetime as datetime
+from scenic_common import get_results
+import pytest
+
+
+with open('./scripts/pg-extender/minDistance.sql', 'r') as file:
+    database.update(file.read())
+
+
+@pytest.mark.parametrize("distance", [ 1, 5, 10, 20, 30, 40, 50 ])
+def test_min_distance(distance):
+    o = objects[0]
+    c = camera
+    results = database.predicate(
+        F.min_distance(c.ego, lit('intersection')) < distance
+    )
+
+    assert set(results) == set(get_results(f"./data/scenic/test-results/min_distance_{distance}.py"))
+    # assert set(results) == set([])
