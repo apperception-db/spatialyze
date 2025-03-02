36ea9b2cc1819b77ab59849ebd1a644bdf22d965
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2025-03-02 06:59:58 +0000

    test

--- tests/interface/utils/F/test_convert_camera.py
+++ tests/interface/utils/F/test_convert_camera.py
@@ -4,9 +4,9 @@ from common import *
 
 @pytest.mark.parametrize("fn, sql", [
     (convert_camera(o, c.ego),
-        "ConvertCamera(t0.translation,c0.egoTranslation,c0.egoHeading)"),
+        "ST_Point((SQRT(POWER((ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.egoTranslation))), 2) + POWER((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.egoTranslation))), 2))) * COS(PI() * (-c0.egoHeading) / 180 + ATAN2((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.egoTranslation))), (ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.egoTranslation))))), (SQRT(POWER((ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.egoTranslation))), 2) + POWER((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.egoTranslation))), 2))) * SIN(PI() * (-c0.egoHeading) / 180 + ATAN2((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.egoTranslation))), (ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.egoTranslation))))))"),
     (convert_camera(o, c),
-        "ConvertCamera(t0.translation,c0.cameraTranslation,c0.cameraHeading)"),
+        "ST_Point((SQRT(POWER((ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.cameraTranslation))), 2) + POWER((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.cameraTranslation))), 2))) * COS(PI() * (-c0.cameraHeading) / 180 + ATAN2((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.cameraTranslation))), (ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.cameraTranslation))))), (SQRT(POWER((ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.cameraTranslation))), 2) + POWER((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.cameraTranslation))), 2))) * SIN(PI() * (-c0.cameraHeading) / 180 + ATAN2((ST_Y(ST_Centroid(t0.translation)) - ST_Y(ST_Centroid(c0.cameraTranslation))), (ST_X(ST_Centroid(t0.translation)) - ST_X(ST_Centroid(c0.cameraTranslation))))))"),
     # (convert_camera(o, o),
     #     "ConvertCamera(t0.translation,t0.translations,timestamp),t0.itemHeading)"),
 ])
--- tests/interface/utils/F/test_road_direction.py
+++ tests/interface/utils/F/test_road_direction.py
@@ -4,13 +4,25 @@ from common import *
 
 @pytest.mark.parametrize("fn, sql", [
     (road_direction(o), 
-        "roadDirection(t0.translation,(t0.itemHeading)::real)"),
+        "ifnull((SELECT CAST(((s_inner.heading * 180 / PI()) + 360) AS numeric) % 360 FROM segment as s_inner WHERE elementId IN (   "
+        "SELECT s_inner2.elementId    FROM SegmentPolygon AS s_inner2    WHERE ST_Covers(s_inner2.elementPolygon, t0.translation)    "
+        "AND (SELECT l.id FROM Lane AS l WHERE l.id = s_inner.elementId) IS NOT NULL) AND ROUND(CAST(s_inner.heading * 180 / PI() AS "
+        "numeric), 3) != -45 ORDER BY st_distance(s_inner.segmentLine, t0.translation), s_inner.elementId ASC LIMIT 1), t0.itemHeading) "),
     (road_direction(o, c.ego), 
-        "roadDirection(t0.translation,(c0.egoHeading)::real)"),
+        "ifnull((SELECT CAST(((s_inner.heading * 180 / PI()) + 360) AS numeric) % 360 FROM segment as s_inner WHERE elementId IN (   "
+        "SELECT s_inner2.elementId    FROM SegmentPolygon AS s_inner2    WHERE ST_Covers(s_inner2.elementPolygon, t0.translation)    "
+        "AND (SELECT l.id FROM Lane AS l WHERE l.id = s_inner.elementId) IS NOT NULL) AND ROUND(CAST(s_inner.heading * 180 / PI() AS "
+        "numeric), 3) != -45 ORDER BY st_distance(s_inner.segmentLine, t0.translation), s_inner.elementId ASC LIMIT 1), c0.egoHeading) "),
     (road_direction(c), 
-        "roadDirection(c0.cameraTranslation,(c0.cameraHeading)::real)"),
+        "ifnull((SELECT CAST(((s_inner.heading * 180 / PI()) + 360) AS numeric) % 360 FROM segment as s_inner WHERE elementId IN (   "
+        "SELECT s_inner2.elementId    FROM SegmentPolygon AS s_inner2    WHERE ST_Covers(s_inner2.elementPolygon, c0.cameraTranslation)    "
+        "AND (SELECT l.id FROM Lane AS l WHERE l.id = s_inner.elementId) IS NOT NULL) AND ROUND(CAST(s_inner.heading * 180 / PI() AS numeric), 3) != -45 "
+        "ORDER BY st_distance(s_inner.segmentLine, c0.cameraTranslation), s_inner.elementId ASC LIMIT 1), c0.cameraHeading) "),
     (road_direction(c.ego, o), 
-        "roadDirection(c0.egoTranslation,(t0.itemHeading)::real)"),
+        "ifnull((SELECT CAST(((s_inner.heading * 180 / PI()) + 360) AS numeric) % 360 FROM segment as s_inner WHERE elementId IN (   "
+        "SELECT s_inner2.elementId    FROM SegmentPolygon AS s_inner2    WHERE ST_Covers(s_inner2.elementPolygon, c0.egoTranslation)    "
+        "AND (SELECT l.id FROM Lane AS l WHERE l.id = s_inner.elementId) IS NOT NULL) AND ROUND(CAST(s_inner.heading * 180 / PI() AS numeric), 3) != -45 "
+        "ORDER BY st_distance(s_inner.segmentLine, c0.egoTranslation), s_inner.elementId ASC LIMIT 1), t0.itemHeading) "),
 ])
 def test_road_direction(fn, sql):
     assert gen(fn) == sql
